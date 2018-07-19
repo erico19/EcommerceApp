60225d91db2815fab3871bf975ef7f35b7d98849
kennethkenzo <nkunyireken@gmail.com>
2018-07-19 09:59:01 +0300

    First commit,User Interface

---
+++ app/src/main/java/com/hansuintern/ecommerceapp/NewOrder.java
@@ -0,0 +1,21 @@
+package com.hansuintern.ecommerceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+    }
+
+    public void chooseModeOfPayment(View view) {
+
+    }
+
+    public void Saved(View view) {
+    }
+}
